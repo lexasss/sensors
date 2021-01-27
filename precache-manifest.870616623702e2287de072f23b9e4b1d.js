c714c80ae2d81150b617d397599f63f432d9ae81
lexasss <oleg.spakov@tuni.fi>
2021-01-27 14:41:46 +0200

    'deploy'

---
+++ precache-manifest.870616623702e2287de072f23b9e4b1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286fdb941bc6e63d1049",
+    "url": "/sensors/css/app.5a9e3c8f.css"
+  },
+  {
+    "revision": "90130dd41b24c75038aaabf81f6ca2c4",
+    "url": "/sensors/img/logo.90130dd4.png"
+  },
+  {
+    "revision": "a81ae7f54427313b09cd9fba3ee62e16",
+    "url": "/sensors/index.html"
+  },
+  {
+    "revision": "286fdb941bc6e63d1049",
+    "url": "/sensors/js/app.8657d2d8.js"
+  },
+  {
+    "revision": "3ab1bc213c3685ed7365",
+    "url": "/sensors/js/chunk-vendors.e1042bf9.js"
+  },
+  {
+    "revision": "1b70ec395ad9f9a18510a62c5c8ecbcb",
+    "url": "/sensors/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sensors/robots.txt"
+  }
+]);
